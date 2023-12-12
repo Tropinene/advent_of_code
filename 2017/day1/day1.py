17a715ba9373fb6e2f25077f92bd8308d8b21cbe
yao <975712228@qq.com>
2023-12-13 00:49:18 +0800

    add 2017 day1

---
+++ 2017/day1/day1.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    line = open("input.txt", 'r').readline().strip()
+
+    p1, p2 = 0, 0
+    length = len(line)
+    offset = int(length / 2)
+    for i in range(length):
+        if line[i] == line[(i + 1) % length]:
+            p1 += int(line[i])
+        if line[i] == line[(i + offset) % length]:
+            p2 += int(line[i])
+
+    print(f"[Part1] : {p1}")
+    print(f"[Part2] : {p2}")
