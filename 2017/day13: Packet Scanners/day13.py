771d5c14a9c0c0c0576bd329f25432f3b20b175a
yao <975712228@qq.com>
2023-12-20 01:34:28 +0800

    finish 2017-day13

--- 2017/day13: Packet Scanners/day13.py
+++ 2017/day13: Packet Scanners/day13.py
@@ -5,6 +5,20 @@ if __name__ == '__main__':
         name, depth = line.strip().split(': ')
         dic[int(name)] = int(depth)
 
-    severity = 0
+    p1 = 0
     for i in dic.keys():
-        pass
+        if i % (2 * dic[i] - 2) == 0:
+            p1 += i * dic[i]
+    print(f"[Part1] : {p1}")
+
+    delay = 1
+    while True:
+        is_caught = False
+        for i in dic.keys():
+            if (i + delay) % (2 * dic[i] - 2) == 0:
+                is_caught = True
+                break
+        if not is_caught:
+            break
+        delay += 1
+    print(f"[Part2] : {delay}")
