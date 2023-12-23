530578d136339a65ae399a1f825d53234d253607
yao <975712228@qq.com>
2023-12-24 04:15:57 +0800

    2018-day4 完成了part1

--- 2018/day4: Repose Record/day4.py
+++ 2018/day4: Repose Record/day4.py
@@ -1,5 +1,5 @@
 import re
-
+from collections import Counter
 
 def procss_logs(logs):
     guard = {}
@@ -9,7 +9,8 @@ def procss_logs(logs):
         if log.endswith("begins shift"):
             match = re.search(r'#(\d+)', log)
             id = int(match.group(1))
-            guard[id] = []
+            if guard.get(id) is None:
+                guard[id] = []
         elif log.endswith("falls asleep"):
             match = re.search(r'(\d{2}-\d{2}) (\d{2}:\d{2})', log).group()
             _, start = match.split()
@@ -17,10 +18,31 @@ def procss_logs(logs):
             match = re.search(r'(\d{2}-\d{2}) (\d{2}:\d{2})', log).group()
             date, end = match.split()
             guard[id].append(date + ' ' + start + ' ' + end)
-    return guard
+
+    filtered_dict = {key: value for key, value in guard.items() if value != []}
+    return filtered_dict
 
 
 if __name__ == '__main__':
     data = open('input.txt', 'r').readlines()
     info = procss_logs(sorted(data))
-    print(info)
+
+    sleep_time, max_id = -1, -1
+    for idx, guard in info.items():
+        time = 0
+        for i in guard:
+            _, s, e = i.split()
+            s, e = int(s.split(':')[1]), int(e.split(':')[1])
+            time += e - s
+        if time > sleep_time:
+            max_id, sleep_time = idx, time
+
+    target = info[max_id]
+    run_dates, sleep_time_cnt = [], Counter()
+    for i in target:
+        date, s, e = i.split()
+        s, e = int(s.split(':')[1]), int(e.split(':')[1])
+        sleep_time_cnt += Counter(range(s, e))
+    most_frequent = sleep_time_cnt.most_common(1)[0][0]
+    print(f"[Part1] : {most_frequent * max_id}")
+
