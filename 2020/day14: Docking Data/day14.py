7bb1cfb161278d2a36fe04d198161aadcee0eb0d
dproot <975712228@qq.com>
2024-01-22 09:52:44 +0800

    add 2020-day14 part2

--- 2020/day14: Docking Data/day14.py
+++ 2020/day14: Docking Data/day14.py
@@ -1,7 +1,10 @@
 import re
+from itertools import product
+
+from typing import List
 
 
-def parse_file():
+def parse_file(is_part2: bool) -> (List[List[tuple]], List[List[tuple]]):
     lines = open('input.txt', 'r').read()
 
     lines_list = lines.split("mask = ")[1:]
@@ -11,8 +14,12 @@ def parse_file():
         mask_str = tmp[0]
         mask = []
         for idx, c in enumerate(mask_str):
-            if c != 'X':
-                mask.append((idx, c))
+            if is_part2:
+                if c != '0':
+                    mask.append((idx, c))
+            else:
+                if c != 'X':
+                    mask.append((idx, c))
         masks.append(mask)
 
         mem = []
@@ -24,8 +31,20 @@ def parse_file():
     return masks, mems
 
 
+def generate_values(input_str: str) -> List[int]:
+    x_count = input_str.count('X')
+    results = []
+    for combination in product(range(2), repeat=x_count):
+        temp_str = input_str
+        for binary_digit in combination:
+            temp_str = temp_str.replace('X', str(binary_digit), 1)
+        value = int(temp_str, 2)
+        results.append(value)
+    return results
+
+
 def main():
-    masks, allmems = parse_file()
+    masks, allmems = parse_file(False)
 
     # Part 1
     men_set_part1 = {}
@@ -40,6 +59,23 @@ def main():
 
     p1 = sum(men_set_part1.values())
     print(f"[Part1] : {p1}")
+
+    # Part 2
+    masks, allmems = parse_file(True)
+    men_set_part2 = {}
+    for idx in range(len(masks)):
+        mask, mems = masks[idx], allmems[idx]
+
+        for (men, value) in mems:
+            men_list = list(bin(men)[2:].zfill(36))
+            for (pos, bit) in mask:
+                men_list[pos] = bit
+            modify_addrs = generate_values(''.join(men_list))
+            for addr in modify_addrs:
+                men_set_part2[addr] = value
+
+    p2 = sum(men_set_part2.values())
+    print(f"[Part2] : {p2}")
 
 
 if __name__ == '__main__':
