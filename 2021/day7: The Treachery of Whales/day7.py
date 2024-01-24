376849df7e937141f0c2e49bfda9e5bfad6d0632
yao <975712228@qq.com>
2024-01-24 22:19:40 +0800

    modify 2020-day7

--- 2021/day7: The Treachery of Whales/day7.py
+++ 2021/day7: The Treachery of Whales/day7.py
@@ -1,12 +1,10 @@
-def count_fuel(hor_pos: int, positions: list, is_part2: bool) -> int:
-    cnt = 0
+def count_fuel(hor_pos: int, positions: list) -> (int, int):
+    cnt, cnt2 = 0, 0
     for i in positions:
-        if is_part2:
-            tmp = abs(i-hor_pos)
-            cnt += (1+tmp) * tmp // 2
-        else:
-            cnt += abs(i-hor_pos)
-    return cnt
+        tmp = abs(i-hor_pos)
+        cnt += tmp
+        cnt2 += (1 + tmp) * tmp // 2
+    return cnt, cnt2
 
 
 def main():
@@ -15,8 +13,7 @@ def main():
 
     p1, p2 = float('inf'), float('inf')
     for i in range(min(positions), max(positions)+1):
-        tmp = count_fuel(i, positions, False)
-        tmp2 = count_fuel(i, positions, True)
+        tmp, tmp2 = count_fuel(i, positions)
         if tmp < p1:
             p1 = tmp
         if tmp2 < p2:
