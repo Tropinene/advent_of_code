14bfd15aac09be94a8ecb1351a7aaeb3ffe7fca7
dproot <975712228@qq.com>
2024-01-26 17:51:59 +0800

    add 2016-day3 part2

--- 2016/day3: Squares With Three Sides/day3.py
+++ 2016/day3: Squares With Three Sides/day3.py
@@ -1,14 +1,31 @@
 # part1: check whether the input 3 numbers can be a triangle
+def is_triangle(sides: list) -> bool:
+    # if lst[0] + lst[1] > lst[2] and lst[1] + lst[2] > lst[0] and lst[0] + lst[2] > lst[1]:
+    if abs(lst[0] - lst[1]) < lst[2] < lst[0] + lst[1]:
+        return True
+
 
 if __name__ == '__main__':
     file_path = './input.txt'
     data = open(file_path, 'r').readlines()
 
-    cnt = 0
+    p1 = 0
     for line in data:
-        lst = line.strip().split()
-        lst = [int(x) for x in lst]
+        lst = [int(x) for x in line.strip().split()]
+        if is_triangle(lst):
+            p1 += 1
+    print(f'[Part1] : {p1}')
 
-        if lst[0] + lst[1] > lst[2] and lst[1] + lst[2] > lst[0] and lst[0] + lst[2] > lst[1]:
-            cnt += 1
-    print(f'[Part1] : {cnt}')
+    vertical = [[], [], []]
+    p2 = 0
+    for line in data:
+        lst = [int(x) for x in line.strip().split()]
+        for i in range(3):
+            vertical[i].append(lst[i])
+    for i in range(3):
+        cur = vertical[i]
+        for i in range(len(cur)):
+            for j in range(i, len(cur)):
+                in_range = sum(1 for num in cur[j+1:] if abs(i-j) < num < i+j)
+                p2 += in_range
+    print(f'[Part2] : {p2}')
