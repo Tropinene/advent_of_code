5f32342afeedbcab8f28a842afe735c41fe745ff
yao <975712228@qq.com>
2024-01-27 14:04:51 +0800

    优化2020-day22 part2

--- 2020/day22: Crab Combat/day22.py
+++ 2020/day22: Crab Combat/day22.py
@@ -27,46 +27,34 @@ def solve1(lst1: list, lst2: list) -> list:
     return player1+player2
 
 
-def sub_game(lst1, lst2) -> bool:
-    player1, player2 = lst1.copy(), lst2.copy()
-    while len(player1) != 0 and len(player2) != 0:
+def sub_game(player1, player2):
+    rounds = set()
+
+    while player1 and player2:
+        current_round = (tuple(player1), tuple(player2))
+
+        if current_round in rounds:
+            return True
+        rounds.add(current_round)
+
         card1, card2 = player1.pop(0), player2.pop(0)
-        if len(player1) == card1 or len(player2) == card2:
-            if sub_game(player1, player2):
-                player1.append(card1)
-                player1.append(card2)
-            else:
-                player2.append(card2)
-                player2.append(card1)
-            continue
-        if card1 > card2:
-            player1.append(card1)
-            player1.append(card2)
+
+        if len(player1) >= card1 and len(player2) >= card2:
+            player1_wins = sub_game(player1[:card1], player2[:card2])
         else:
-            player2.append(card2)
-            player2.append(card1)
-    return len(player1) > len(player2)
+            player1_wins = card1 > card2
+
+        if player1_wins:
+            player1.extend([card1, card2])
+        else:
+            player2.extend([card2, card1])
+
+    return bool(player1)
 
 
-def solve2(lst1: list, lst2: list) -> list:
-    player1, player2 = lst1.copy(), lst2.copy()
-    while len(player1) != 0 and len(player2) != 0:
-        card1, card2 = player1.pop(0), player2.pop(0)
-        if len(player1) == card1 or len(player2) == card2:
-            if sub_game(player1, player2):
-                player1.append(card1)
-                player1.append(card2)
-            else:
-                player2.append(card2)
-                player2.append(card1)
-            continue
-        if card1 > card2:
-            player1.append(card1)
-            player1.append(card2)
-        else:
-            player2.append(card2)
-            player2.append(card1)
-    return player1+player2
+def solve2(player1, player2):
+    sub_game(player1, player2)
+    return player1 + player2
 
 
 def main():
