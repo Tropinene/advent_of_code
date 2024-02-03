3be8924aabd162a210e2f9c846bdc1144caccc25
yao <975712228@qq.com>
2024-02-03 15:41:02 +0800

    add 2022-day20

--- 2022/day20: Grove Positioning System/day20.py
+++ 2022/day20: Grove Positioning System/day20.py
@@ -1,22 +1,31 @@
-def parse_file() -> list:
-    data = list(map(int, open('input.txt', 'r').readlines()))
-    res = []
-    for idx, val in enumerate(data):
-        res.append((idx, val))
-    return res
+from collections import deque
 
 
-def mix(nums: list) -> list:
-    l = len(nums)
-    for idx, val in nums:
-        new_idx = (idx + val + l) % l
-        print(nums)
-        quit()
+def mix(lst, rounds) -> int:
+    length = len(lst)
+    zero = (0, lst.index(0))
+    data = deque(
+        [(number, index) for index, number in enumerate(lst)]
+    )
+    order = [n for n in lst]
+    for _ in range(rounds):
+        for i, number in enumerate(order):
+            index = data.index((number, i))
+            data.rotate(-index)
+            value: tuple[int, int] = data.popleft()
+            data.rotate(-value[0])
+            data.appendleft(value)
+
+    zero_index = data.index(zero)
+    return sum(data[(zero_index + i) % length][0] for i in (1000, 2000, 3000))
 
 
 def main():
-    numbers = parse_file()
-    mix(numbers)
+    numbers = [int(x) for x in open('input.txt', 'r').readlines()]
+    print(f"[Part1] : {mix(numbers, 1)}")
+
+    numbers = [n*811589153 for n in numbers]
+    print(f"[Part1] : {mix(numbers, 10)}")
 
 
 if __name__ == '__main__':
