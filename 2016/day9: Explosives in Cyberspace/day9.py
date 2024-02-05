621b55554371da3b7018dee493c5fd0426e89fe1
dproot <975712228@qq.com>
2024-02-05 17:47:52 +0800

    add 2016-day9 part1

--- 2016/day9: Explosives in Cyberspace/day9.py
+++ 2016/day9: Explosives in Cyberspace/day9.py
@@ -21,10 +21,29 @@ def cnt_length(s: str) -> int:
     return len(res)
 
 
+def cnt_length2(s: str) -> int:
+    print(s)
+    cur_ = 0
+    left = s.find('(', cur_)
+    if left == -1:
+        return len(s)
+    right = s.find(')', cur_)
+
+    char_num, repeat_times = s[left + 1:right].split('x')
+    char_num, repeat_times = int(char_num), int(repeat_times)
+    # print(s[:left])
+    # print(s[right+char_num+1:])
+    return cnt_length2(s[:left]) + cnt_length2(s[right+1:right+char_num+1]) + cnt_length2(s[right+char_num+1:])
+
+
 def main():
-    line = open('input.txt', 'r').readline().strip()
+    # line = open('input.txt', 'r').readline().strip()
+    line = "(6x1)(1x3)A"
     p1 = cnt_length(line)
     print(f"[Part1] : {p1}")
+
+    p2 = cnt_length2(line)
+    print(f"[Part2] : {p2}")
 
 
 if __name__ == '__main__':
