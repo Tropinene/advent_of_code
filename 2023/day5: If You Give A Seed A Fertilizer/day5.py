ae8e79a653468f46cec859bbfb0e5c6f6b6f66f9
yao <975712228@qq.com>
2024-09-04 12:15:35 -0400

    简化了 day5 part1

--- 2023/day5: If You Give A Seed A Fertilizer/day5.py
+++ 2023/day5: If You Give A Seed A Fertilizer/day5.py
@@ -7,29 +7,23 @@ def mapping(target: int, relation: list) -> int:
 
 
 def main():
-    seeds, seed2soil, soil2fert, fert2water, water2light, light2temp, temp2humi, humi2loca \
-        = open('input.txt', 'r').read().split('\n\n')
-    seed2soil = seed2soil.split('\n')[1:]
-    soil2fert = soil2fert.split('\n')[1:]
-    fert2water = fert2water.split('\n')[1:]
-    water2light = water2light.split('\n')[1:]
-    light2temp = light2temp.split('\n')[1:]
-    temp2humi = temp2humi.split('\n')[1:]
-    humi2loca = humi2loca.split('\n')[1:]
+    maps = open('input.txt', 'r').read().split('\n\n')
+    seeds = maps[0]
+    maps = maps[1:]
+    final_maps = []
+    for m in maps:
+        m = m.split('\n')[1:]
+        final_maps.append(m)
 
     num_seeds = [int(x) for x in seeds[7:].split()]
     min_location = 10000000000
 
     for s in num_seeds:
-        soil = mapping(s, seed2soil)
-        fert = mapping(soil, soil2fert)
-        water = mapping(fert, fert2water)
-        light = mapping(water, water2light)
-        temp = mapping(light, light2temp)
-        humi = mapping(temp, temp2humi)
-        location = mapping(humi, humi2loca)
-        min_location = min(min_location, location)
-    print(min_location)
+        tmp = s
+        for m in final_maps:
+            tmp = mapping(tmp, m)
+        min_location = min(min_location, tmp)
+    print(f"[Part1] : {min_location}")
 
 
 if __name__ == '__main__':
