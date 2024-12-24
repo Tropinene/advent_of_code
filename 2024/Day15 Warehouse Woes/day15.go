b1ca5c0841450eb10460c34a79f95577876a86b2
yao <975712228@qq.com>
2024-12-23 20:46:54 -0500

    solve 2024-day15 part1

--- 2024/Day15 Warehouse Woes/day15.go
+++ 2024/Day15 Warehouse Woes/day15.go
@@ -49,14 +49,11 @@ func countDis(graph [][]byte) int {
 	return count
 }
 
-func solve(graph [][]byte, moves string) int {
+func solve1(graph [][]byte, moves string) int {
 	rows, cols := len(graph), len(graph[0])
 	curR, curC := findRobot(graph)
 
 	for _, move := range moves {
-		//fmt.Println(string(move))
-		//printG(graph)
-		//fmt.Println()
 		nextC, nextR := curC, curR
 		if move == '<' {
 			tmp := curC - 1
@@ -121,6 +118,112 @@ func solve(graph [][]byte, moves string) int {
 	return countDis(graph)
 }
 
+func expandMap(graph [][]byte) [][]byte {
+	rows, cols := len(graph), len(graph[0])
+	newGraph := make([][]byte, rows)
+	for i := range newGraph {
+		newGraph[i] = make([]byte, cols*2)
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			switch graph[i][j] {
+			case '#':
+				newGraph[i][j*2] = '#'
+				newGraph[i][j*2+1] = '#'
+			case 'O':
+				newGraph[i][j*2] = '['
+				newGraph[i][j*2+1] = ']'
+			case '.':
+				newGraph[i][j*2] = '.'
+				newGraph[i][j*2+1] = '.'
+			case '@':
+				newGraph[i][j*2] = '@'
+				newGraph[i][j*2+1] = '.'
+			}
+		}
+	}
+	return newGraph
+}
+
+func countDisWide(graph [][]byte) int {
+	rows, cols := len(graph), len(graph[0])
+	count := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols-1; j++ {
+			if graph[i][j] == '[' && graph[i][j+1] == ']' {
+				count += i*100 + j
+			}
+		}
+	}
+	return count
+}
+
+func solve2(graph [][]byte, moves string) int {
+	expandedGraph := expandMap(graph)
+
+	rows, cols := len(expandedGraph), len(expandedGraph[0])
+	curR, curC := findRobot(expandedGraph)
+
+	for _, move := range moves {
+		printG(expandedGraph)
+		fmt.Println()
+		nextC, nextR := curC, curR
+		if move == '<' {
+			tmp := curC - 1
+			for tmp > 0 && (expandedGraph[curR][tmp] == '[' || expandedGraph[curR][tmp] == ']') {
+				tmp -= 1
+			}
+			if tmp == 0 || expandedGraph[curR][tmp] == '#' {
+				continue
+			} else {
+				for tmp < curC {
+					expandedGraph[curR][tmp] = expandedGraph[curR][tmp+1]
+					tmp += 1
+				}
+				nextC -= 1
+			}
+		} else if move == '>' {
+			tmp := curC + 1
+			for tmp < cols && (expandedGraph[curR][tmp] == '[' || expandedGraph[curR][tmp] == ']') {
+				tmp += 1
+			}
+			if tmp == cols || expandedGraph[curR][tmp] == '#' {
+				continue
+			} else {
+				for tmp > curC {
+					expandedGraph[curR][tmp] = expandedGraph[curR][tmp-1]
+					tmp -= 1
+				}
+				nextC += 1
+			}
+		} else if move == '^' {
+			tmp := curR - 1
+			for tmp > 0 && (expandedGraph[tmp][curC] == '[' || expandedGraph[tmp][curC] == ']') {
+				tmp -= 1
+			}
+			if tmp == 0 || expandedGraph[tmp][curC] == '#' {
+				continue
+			} else {
+				// todo: fix this
+			}
+		} else if move == 'v' {
+			tmp := curR + 1
+			for tmp < rows && (expandedGraph[tmp][curC] == '[' || expandedGraph[tmp][curC] == ']') {
+				tmp += 1
+			}
+			if tmp == rows || expandedGraph[tmp][curC] == '#' {
+				continue
+			} else {
+				// todo fix this
+			}
+		}
+		expandedGraph[curR][curC] = '.'
+		curR, curC = nextR, nextC
+	}
+	return countDisWide(expandedGraph)
+}
+
 func main() {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(filename)
@@ -131,6 +234,21 @@ func main() {
 		graph[i] = []byte(str)
 	}
 
-	p1 := solve(graph, moves)
+	p1 := solve1(graph, moves)
 	fmt.Println("[Part 1]:", p1)
+	//p2 := solve2(graph, moves)
+	tmpG := [][]byte{
+		[]byte("####################"),
+		[]byte("##[].......[].[][]##"),
+		[]byte("##[]...........[].##"),
+		[]byte("##[]........[][][]##"),
+		[]byte("##[]......[]....[]##"),
+		[]byte("##..##......[]....##"),
+		[]byte("##..[]............##"),
+		[]byte("##..@......[].[][]##"),
+		[]byte("##......[][]..[]..##"),
+		[]byte("####################"),
+	}
+	p2 := countDisWide(tmpG)
+	fmt.Println("[Part 2]:", p2)
 }
