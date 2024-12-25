ed210aef573a1e0a77498aab575e9c8b1d835916
yao <975712228@qq.com>
2024-12-25 01:06:17 -0500

    solve 2024-day22 part1

--- 2024/Day22 Monkey Market/day22.go
+++ 2024/Day22 Monkey Market/day22.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -17,9 +18,25 @@ func readFile(fileName string) []string {
 	return strings.Split(strings.TrimSpace(string(data)), "\n")
 }
 
+func solve1(secrets []string) int {
+	ans := 0
+	for _, secret := range secrets {
+		num, _ := strconv.Atoi(secret)
+		for i := 0; i < 2000; i++ {
+			num = ((num << 6) ^ num) & 0xFFFFFF
+			num = ((num >> 5) ^ num) & 0xFFFFFF
+			num = ((num << 11) ^ num) & 0xFFFFFF
+		}
+		ans += num
+	}
+	return ans
+}
+
 func main() {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(filename)
 	lines := readFile(filepath.Join(dir, "input.txt"))
 
+	p1 := solve1(lines)
+	fmt.Println("[Part 1]:", p1)
 }
