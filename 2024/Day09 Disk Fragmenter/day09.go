ad3f40d0fe0ac61a2409398bf0bccbacd32f8a02
yao <975712228@qq.com>
2024-12-27 11:33:15 -0500

    solve 2024-day9 part1

--- 2024/Day09 Disk Fragmenter/day09.go
+++ 2024/Day09 Disk Fragmenter/day09.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -17,9 +18,48 @@ func readFile(fileName string) []string {
 	return strings.Split(strings.TrimSpace(string(data)), "\n")
 }
 
+func solve1(s string) int {
+	var res []int
+	flag, ans := 0, 0
+	for i, c := range s {
+		n, _ := strconv.Atoi(string(c))
+		if i%2 == 0 {
+			for j := 0; j < n; j++ {
+				res = append(res, flag)
+			}
+			flag += 1
+		} else {
+			for j := 0; j < n; j++ {
+				res = append(res, -1)
+			}
+		}
+	}
+
+	i, j := 0, len(res)-1
+	for i != j {
+		for i < j && res[i] != -1 {
+			i++
+		}
+		for j > i && res[j] == -1 {
+			j--
+		}
+		res[i], res[j] = res[j], res[i]
+	}
+
+	for k := 0; k < len(res); k++ {
+		if res[k] == -1 {
+			break
+		}
+		ans += res[k] * k
+	}
+	return ans
+}
+
 func main() {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(filename)
 	lines := readFile(filepath.Join(dir, "input.txt"))
 
+	p1 := solve1(lines[0])
+	fmt.Println("[Part 1]:", p1)
 }
