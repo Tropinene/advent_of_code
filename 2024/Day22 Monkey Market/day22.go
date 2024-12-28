2e67f680eedd5074db7d355aea008684f0565775
yao <975712228@qq.com>
2024-12-27 20:21:52 -0500

    solve 2024-day22

--- 2024/Day22 Monkey Market/day22.go
+++ 2024/Day22 Monkey Market/day22.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -18,18 +19,61 @@ func readFile(fileName string) []string {
 	return strings.Split(strings.TrimSpace(string(data)), "\n")
 }
 
+func hash(num int) int {
+	num = ((num << 6) ^ num) & 0xFFFFFF
+	num = ((num >> 5) ^ num) & 0xFFFFFF
+	num = ((num << 11) ^ num) & 0xFFFFFF
+	return num
+}
+
 func solve1(secrets []string) int {
 	ans := 0
 	for _, secret := range secrets {
 		num, _ := strconv.Atoi(secret)
 		for i := 0; i < 2000; i++ {
-			num = ((num << 6) ^ num) & 0xFFFFFF
-			num = ((num >> 5) ^ num) & 0xFFFFFF
-			num = ((num << 11) ^ num) & 0xFFFFFF
+			num = hash(num)
 		}
 		ans += num
 	}
 	return ans
+}
+
+func extractSequences(secret int) map[[4]int]int {
+	var res = make(map[[4]int]int)
+	var diff []int
+
+	for i := 0; i < 2000; i++ {
+		var newSecret = hash(secret)
+		diff = append(diff, (newSecret%10)-(secret%10))
+		if len(diff) > 4 {
+			diff = diff[1:]
+		}
+		if len(diff) == 4 {
+			key := [4]int{diff[0], diff[1], diff[2], diff[3]}
+			if _, exists := res[key]; !exists {
+				res[key] = newSecret % 10
+			}
+		}
+		secret = newSecret
+	}
+	return res
+}
+
+func solve2(secrets []string) int {
+	var sequences = make(map[[4]int]int)
+	for _, secret := range secrets {
+		num, _ := strconv.Atoi(secret)
+		for seq, price := range extractSequences(num) {
+			sequences[seq] += price
+		}
+	}
+	var res = math.MinInt
+	for _, v := range sequences {
+		if v > res {
+			res = v
+		}
+	}
+	return res
 }
 
 func main() {
@@ -39,4 +83,6 @@ func main() {
 
 	p1 := solve1(lines)
 	fmt.Println("[Part 1]:", p1)
+	p2 := solve2(lines)
+	fmt.Println("[Part 2]:", p2)
 }
